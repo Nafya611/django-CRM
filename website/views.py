93ee0afc9fa7bf6b1f0a7682bfa12cdb82b3d7ad
Nafyad Hailu <nafyadhailu99@gmail.com>
2024-11-12 16:08:27 +0300

    add register template and edit  urls and views

--- website/views.py
+++ website/views.py
@@ -26,5 +26,7 @@ def logout_user(request):
     logout(request)   
     messages.success(request,'You have been logeed out...') 
     return redirect('home')
+def register_user(request):
+    return render(request,'register.html',{})
 
-
+ 
